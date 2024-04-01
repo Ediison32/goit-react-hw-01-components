bd018b026ef8b2f6fc74b0dbcc57cc085fb00ac1
Ediison32 <edisonalvarez32@gmail.com>
2024-03-31 19:50:30 -0500

    backgroundColor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        backgroundColor: '#f5f5f5',
       }}
     >
       React homework template
